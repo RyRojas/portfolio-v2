49e2fcca7d8354d357a119b939f142b3bfbca905
RyRojas <30281811+RyRojas@users.noreply.github.com>
2021-08-22 13:42:38 -0700

    Added project data

---
+++ components/data/project-data.js
@@ -0,0 +1,77 @@
+export const projects = [
+  {
+    name: 'PokéDex',
+    description:
+      'A simple PokéDex app to get familiarized with JavaScript and Bootstrap, as well as working with an API.',
+    tech: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
+    links: [
+      {
+        type: 'Github',
+        url: 'https://github.com/RyRojas/js-pokedex',
+      },
+      {
+        type: 'Demo',
+        url: 'https://ryrojas.github.io/js-pokedex/',
+      },
+    ],
+  },
+  {
+    name: 'theMovieBook',
+    description:
+      'Movie database single-page application. Database and RESTful API built from scratch with the client being built twice using both React and Angular.',
+    tech: ['MongoDB', 'Express', 'React', 'Node.js', 'Bootstrap', 'SCSS'],
+    links: [
+      {
+        type: 'Github',
+        text: 'Backend',
+        url: 'https://github.com/RyRojas/themoviebook-api',
+      },
+      {
+        type: 'Github',
+        text: 'Frontend',
+        url: 'https://github.com/RyRojas/themoviebook-client',
+      },
+      {
+        type: 'Demo',
+        url: 'https://themoviebook.netlify.app/',
+      },
+    ],
+  },
+  {
+    name: 'MeetApp',
+    description:
+      'Simple calendar app for developer events built with a test-driven development approach, including unit, integration, and E2E tests. AWS Lambda used for OAuth authentication and Google Calendar integration. Makes use of local caching and service workers for offline and mobile use.',
+    tech: ['React', 'AWS Lambda', 'Jest', 'Enzyme', 'CSS'],
+    links: [
+      {
+        type: 'Github',
+        url: 'https://github.com/RyRojas/meetapp',
+      },
+      {
+        type: 'Demo',
+        url: 'https://ryrojas.github.io/meetapp/',
+      },
+    ],
+  },
+  {
+    name: 'Chatify',
+    description:
+      'React Native chat app that uses Firebase and Firestore for authentication and data storage. Makes use of async storage for local data storage and offline functionality. Built to a design specification.',
+    tech: [
+      'React Native',
+      'Firebase/Firestore',
+      'Async Storage',
+      'Gifted Chat',
+    ],
+    links: [
+      {
+        type: 'Github',
+        url: 'https://github.com/RyRojas/meetapp',
+      },
+      {
+        type: 'Demo',
+        url: 'https://ryrojas.github.io/meetapp/',
+      },
+    ],
+  },
+];
