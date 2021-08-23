b195da1bdf8536c654867c8d2630058011e6d729
RyRojas <30281811+RyRojas@users.noreply.github.com>
2021-08-22 17:43:43 -0700

    Built hero component

---
+++ components/hero.jsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import headshot from '../public/images/headshot.png';
+import styles from '../styles/Hero.module.css';
+
+export default function Hero() {
+  return (
+    <section className={styles.heroSection}>
+      <div className={styles.heroContainer}>
+        <Image
+          src={headshot}
+          alt="My beautiful face smiling back at you."
+          height={200}
+          width={200}
+          priority={true}
+        />
+        <div className={styles.heroText}>
+          <h1>Hello</h1>
+          <div>
+            <p>
+              I{`'`}m Ryan, a student <span>web</span> developer based in
+              Portland, OR
+            </p>
+          </div>
+          <a href="#">See some of my previous work.</a>
+        </div>
+      </div>
+    </section>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import Hero from '../components/hero';
 import Layout from '../components/layout';
 
 export default function Home() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <Hero />
+    </Layout>
+  );
 }
