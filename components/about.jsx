1cae9313612afbe3624ae4b7793a834c8daecf7d
RyRojas <30281811+RyRojas@users.noreply.github.com>
2021-08-25 17:24:06 -0700

    Added skills to about section

--- components/about.jsx
+++ components/about.jsx
@@ -1,6 +1,25 @@
 import styles from '../styles/About.module.css';
 
 export default function About() {
+  const SKILLS = [
+    'React',
+    'Redux',
+    'JavaScript',
+    'TypeScript',
+    'jQuery',
+    'Angular',
+    'Node.js',
+    'Express',
+    'Git',
+    'MongoDB',
+    'postgreSQL',
+    'AWS',
+    'HTML',
+    'CSS',
+    'Bootstrap',
+    'Material UI',
+  ];
+
   return (
     <section id="about" className={styles.about}>
       <div className={styles.aboutContainer}>
@@ -11,11 +30,19 @@ export default function About() {
           in different frontend frameworks to go along with various pet backend
           projects.
         </p>
-
         <p>
           In my free time, I enjoy riding public transit, trying new food, and
           travelling to partake in the prior two.
         </p>
+      </div>
+
+      <div className={styles.skillsContainer}>
+        <h3>What I{`'`}ve been working with lately</h3>
+        <ul>
+          {SKILLS.map((skill, i) => (
+            <li key={i}>{skill}</li>
+          ))}
+        </ul>
       </div>
     </section>
   );
