7192130206c8bc0f6ddfc373ed00889f11cd3e10
RyRojas <30281811+RyRojas@users.noreply.github.com>
2021-08-26 23:16:35 -0700

    Finalized structure and style of header

--- components/header.jsx
+++ components/header.jsx
@@ -1,34 +1,51 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import logo from '../public/images/logo.png';
 import styles from '../styles/Header.module.css';
+import { useRouter } from 'next/dist/client/router';
 
 export default function Header() {
+  const router = useRouter();
+
   return (
     <header className={styles.header}>
-      <Image src={logo} alt="My logo." />
+      <Link href="/" passHref>
+        <a>
+          <Image
+            src={logo}
+            layout="fixed"
+            height={75}
+            width={75}
+            alt="My logo."
+          />
+        </a>
+      </Link>
+
       <nav>
-        <ul role="menubar" className={styles.navMenu}>
-          <li role="presentation" className={styles.navMenuItem}>
-            <a href="#" role="menuitem">
-              Home
-            </a>
-          </li>
-          <li role="presentation" className={styles.navMenuItem}>
-            <a href="#about" role="menuitem">
-              About
-            </a>
-          </li>
-          <li role="presentation" className={styles.navMenuItem}>
-            <a href="#projects" role="menuitem">
-              Work
-            </a>
-          </li>
-          <li role="presentation" className={styles.navMenuItem}>
-            <a href="#contact" role="menuitem">
-              Contact
-            </a>
-          </li>
-        </ul>
+        {router.pathname === '/' ? (
+          <ul role="menubar" className={styles.navMenu}>
+            <li role="presentation" className={styles.navMenuItem}>
+              <Link href="/" role="menuitem">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li role="presentation" className={styles.navMenuItem}>
+              <a href="#about" role="menuitem">
+                About
+              </a>
+            </li>
+            <li role="presentation" className={styles.navMenuItem}>
+              <a href="#projects" role="menuitem">
+                Work
+              </a>
+            </li>
+            <li role="presentation" className={styles.navMenuItem}>
+              <a href="#contact" role="menuitem">
+                Contact
+              </a>
+            </li>
+          </ul>
+        ) : null}
       </nav>
     </header>
   );
